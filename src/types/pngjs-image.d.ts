471fec7c43db034817e643883bf826df4ac49a85
TheProgrammingGiantPanda <theprogrammingiantpanda@gmail.com>
2026-02-02 01:47:07 +0000

    AI - Restore pngjs-image type declaration and clean up stray JS files

--- src/types/pngjs-image.d.ts
+++ src/types/pngjs-image.d.ts
@@ -1,27 +1,3 @@
 declare module 'pngjs-image' {
-  interface Color {
-    red: number;
-    green: number;
-    blue: number;
-    alpha?: number;
-  }
-
-  interface PNGImage {
-    getIndex(x: number, y: number): number;
-    getRed(idx: number): number;
-    getGreen(idx: number): number;
-    getBlue(idx: number): number;
-    getAlpha(idx: number): number;
-    setAt(x: number, y: number, color: Color): void;
-    fillRect(x: number, y: number, w: number, h: number, color: Color): void;
-    writeImage(filename: string, callback?: (err?: Error) => void): void;
-  }
-
-  interface CreateImageFunction {
-    (width: number, height: number): PNGImage;
-    createImage(width: number, height: number): PNGImage;
-  }
-
-  const createImage: CreateImageFunction;
-  export = createImage;
+  export default function PNGImage(width: number, height: number): any;
 }
